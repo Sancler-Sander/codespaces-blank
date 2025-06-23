40c5b431d6f1bab29750779d0be5431c25a9b049
Sancler-Sander <san.sander@hotmail.com>
2025-06-23 18:57:30 -0300

    Update xadrez_nivel-mestre.c

--- xadrez_nivel-mestre.c
+++ xadrez_nivel-mestre.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 /*
-Recursividade: Substitua os loops simples que controlam os movimentos da Torre, Bispo e Rainha por funções recursivas. Cada função recursiva deverá simular o movimento da respectiva peça, imprimindo a direção correta a cada casa.
+Recursividade: Substitua os loops simples que controlam os movimentos da Torre, Bispo e Rainha por funções recursivas. 
+Cada função recursiva deverá simular o movimento da respectiva peça, imprimindo a direção correta a cada casa.
+
  
-Loops Complexos para o Cavalo: Aprimore a movimentação do Cavalo, utilizando loops aninhados com múltiplas variáveis e/ou condições. Você pode usar continue e break para controlar o fluxo do loop de forma mais precisa. O movimento do Cavalo agora será para cima e para a direita, em "L" (duas casas para cima e uma para a direita).
+Loops Complexos para o Cavalo: Aprimore a movimentação do Cavalo, utilizando loops aninhados com múltiplas variáveis e/ou condições. 
+Você pode usar continue e break para controlar o fluxo do loop de forma mais precisa. 
+O movimento do Cavalo agora será para cima e para a direita, em "L" (duas casas para cima e uma para a direita).
  
-Bispo com Loops Aninhados: O Bispo deve ser implementado com recursividade, e também com loops aninhados, utilizando o loop mais externo para o movimento vertical, e o mais interno para o movimento horizontal. */
+Bispo com Loops Aninhados: O Bispo deve ser implementado com recursividade, e também com loops aninhados, 
+utilizando o loop mais externo para o movimento vertical, e o mais interno para o movimento horizontal. */
+
+void 
